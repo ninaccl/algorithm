bf8642851e7a55e6016718d227bdffac9f2fe13f
ninaccl <30490041+ninaccl@users.noreply.github.com>
2019-11-20 20:06:27 +0800

    add some print algorithms
    1.利用全局的概念转圈打印矩阵
    2.利用全局的概念之字形打印矩阵

---
+++ 算法题/之字形打印矩阵/main.cpp
@@ -0,0 +1,58 @@
+﻿//“之”字形打印矩阵
+//【题目】
+//给定一个矩阵matrix，按照“之”字形的方式打印这个矩阵，例如：
+//1 2 3 4
+//5 6 7 8
+//9 10 11 12
+//“之”字形打印的结果为：1，2，5，9，6，3，4，7，10，11，8，12
+//【要求】
+//额外空间复杂度为O(1)。
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+void printLevel(int **m, int row1, int col1, int row2, int col2,int colLength, bool fromUp)
+{
+	if(fromUp)//自上向下打印
+		while (row1 <= row2)
+		{
+			printf("%d ", *((int *)m + row1*colLength + col1));
+			row1++;
+			col1--;
+		}
+	else//自下向上打印
+	{
+		while (row2 >= row1)
+		{
+			printf("%d ", *((int *)m + row2*colLength + col2));
+			row2--;
+			col2++;
+		}
+	}
+}
+
+void printMatrixZigZag(int **m,int rowLength,int colLength)
+{
+	int row1 = 0, col1 = 0, row2 = 0, col2 = 0;
+	int endR = rowLength - 1;
+	int endC = colLength - 1;
+	bool fromUp = false;
+	while (row1 != endR + 1)
+	{
+		printLevel(m,row1,col1,row2,col2,colLength,fromUp);
+		row1 = col1 == endC ? row1 + 1 : row1;
+		col1 = col1 == endC ? col1 : col1 + 1;
+		col2 = row2 == endR ? col2 + 1 : col2;
+		row2 = row2 == endR ? row2 : row2 + 1;	
+		fromUp = !fromUp;
+	}
+}
+
+void main()
+{
+	int m[3][4] = { {1,2,3,4},{5,6,7,8},{9,10,11,12} };
+	int rowLength = 3, colLength = 4;
+	printMatrixZigZag((int**)m, rowLength, colLength);
+	system("pause");
+}
---
+++ 算法题/转圈打印矩阵/main.cpp
@@ -0,0 +1,66 @@
+﻿//转圈打印矩阵
+//【题目】
+//给定一个整型矩阵matrix，请按照转圈的方式打印它。
+//例如：
+//1 2 3 4
+//5 6 7 8
+//9 10 11 12
+//13 14 15 16
+//打印结果为：1，2，3，4，8，12，16，15，14，13，9，5，6，7，11，
+//10
+//【要求】
+//额外空间复杂度为O(1)。
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+void printEdge(int** m, int row1, int col1, int row2, int col2,int colLength)
+{
+	if (row1 == row2)
+		for (int i = col1; i < col2; i++)
+			printf("%d ", *((int *)m + i));
+	else if (col1 == col2)
+		for (int i = row1; i < row2; i++)
+			printf("%d ", *((int *)m + i*(col2 - col1 + 1)));
+	else
+	{
+		int curC = col1;
+		int curR = row1;
+		while (curC != col2)
+		{
+			printf("%d ", *((int *)m + row1*colLength + curC));
+			curC++;
+		}
+		while (curR != row2)
+		{
+			printf("%d ", *((int *)m + curR*colLength + col2));
+			curR++;
+		}
+		while (curC != col1)
+		{
+			printf("%d ", *((int *)m + row2*colLength + curC));
+			curC--;
+		}
+		while (curR != row1)
+		{
+			printf("%d ", *((int *)m + curR*colLength + col1));
+			curR--;
+		}
+	}
+}
+
+void spiralOrderPrint(int** matrix,int rowLength,int colLength)
+{
+	int row1 = 0, col1 = 0, row2 = rowLength - 1, col2 = colLength - 1;
+	while (row1 <= row2&&col1 <= col2)
+		printEdge(matrix, row1++, col1++, row2--, col2--,colLength);
+}
+
+void main()
+{
+	int m[4][4] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
+	int rowLength = 4;
+	int colLength = 4;
+	spiralOrderPrint((int **)m,rowLength,colLength);
+	system("pause");
+}
